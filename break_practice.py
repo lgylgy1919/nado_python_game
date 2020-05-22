a373f1818a1cde0b6ef6c4d857a5a500908d4baa
lgylgy1919 <59249964+lgylgy1919@users.noreply.github.com>
2020-05-22 23:28:52 +0900

    game_project(1)

---
+++ break_practice.py
@@ -0,0 +1,14 @@
+balls = [1, 2, 3, 4]
+weapons = [11, 22, 3, 44]
+
+for ball_idx, ball_val in enumerate(balls):
+    print("ball:", ball_val)
+    for weapn_idx, weapon_val in enumerate(weapons):
+        print("weapons :", weapon_val)
+        if ball_val == weapon_val:  # 충돌체크
+            print("공과 무기가 충돌")
+            break
+
+    else:
+        continue
+    break
